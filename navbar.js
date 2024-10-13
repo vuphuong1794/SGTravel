f180275cbedd259102145e0564e6bdc78d382095
MaiKhoa0101 <maikhoa2015@gmail.com>
2024-10-13 11:55:30 +0700

    update main,css, navbar,footer, themecolor

--- navbar.js
+++ navbar.js
@@ -4,13 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     navbarCSS.href = 'style/navbar.css';
     document.head.appendChild(navbarCSS);
     const navbar = `
-        <div class="navbar">
+        <div class="navbar" id="navbar">
             <div class="nav-container">
                 <h2 class="nav-logo"><b>SGTravel</b></h2>
                 <div class="searchbox-container">
                     <input type="text" id="searchbox" placeholder="Tìm kiếm" />
                     <box-icon name='search-alt-2' class="icon"></box-icon>
                 </div>
+                <label class="switch">
+                    <input type="checkbox" id="theme-toggle">
+                    <span class="slider-navbar" for="theme-toggle"></span>
+                </label>
                 <div class="user-container">
                     <button class="signUp-btn"><a href="Signup.html" style="text-decoration: none; color: black">Đăng ký</a></button>
                     <button class="login-btn"><a href="Login.html" style="text-decoration: none; color: black">Đăng nhập</a></button>
