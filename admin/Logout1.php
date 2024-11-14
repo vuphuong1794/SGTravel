17702f2778e5f792e175283bdcf72b6266b8497e
vuphuong1794 <phuongvu1794@gmail.com>
2024-11-14 10:57:10 +0700

    delete cookie when logout in admin page

--- admin/Logout1.php
+++ admin/Logout1.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 
+// Xóa tất cả cookie liên quan đến đăng nhập
+if (isset($_COOKIE['user_login'])) {
+    unset($_COOKIE['user_login']);
+    setcookie('user_login', '', time() - 3600, '/'); // Xóa cookie bằng cách set thời gian hết hạn trong quá khứ
+}
+
 // Xóa tất cả dữ liệu trong session
 session_unset();
 
